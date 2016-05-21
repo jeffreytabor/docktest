f4ac96c059fbaef23efb189e3131c20e0cfe9390
Jeff Tabor <jeffreytabor@gmail.com>
2016-05-20 20:58:41 -0400

    php info

--- dock.php
+++ dock.php
@@ -1,9 +1,15 @@
 <?php
 
   /**
-   * commenting now works
+   * List items to set up FarCry demo:
    *
-   * List items to set up FarCry demo
+   * Set up two Farcry instances, 'A' and 'B', 
+   * point both to the same version 6 core, and then,
+   * point one to another, updated version 6 core,
+   * finally move second site to newer core.
+   * 
    */
+
+  echo phpinfo();
 
 ?>
